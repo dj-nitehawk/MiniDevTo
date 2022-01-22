4f9b46d4b767d4c9370020baa982e21209887602
Damith G <djnitehawk83@gmail.com>
2022-01-22 20:11:51 +0530

    upgrade to fastendpoints 3

--- Program.cs
+++ Program.cs
@@ -3,27 +3,20 @@ global using FastEndpoints.Security;
 global using FastEndpoints.Validation;
 global using MiniDevTo.Auth;
 global using MongoDB.Entities;
-using FastEndpoints.Swashbuckle;
-using Microsoft.AspNetCore.Http.Json;
+using FastEndpoints.Swagger;
 using MiniDevTo.Migrations;
-using Swashbuckle.AspNetCore.SwaggerUI;
 
 var builder = WebApplication.CreateBuilder();
-builder.Services.Configure<JsonOptions>(o => o.SerializerOptions.PropertyNamingPolicy = null); //pascal case for serialization
 builder.Services.AddFastEndpoints();
 builder.Services.AddAuthenticationJWTBearer(builder.Configuration["JwtSigningKey"]);
-builder.Services.AddSwashbuckle();
+builder.Services.AddSwaggerDoc();
 
 var app = builder.Build();
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseFastEndpoints();
-app.UseSwagger();
-app.UseSwaggerUI(o =>
-{
-    o.DocExpansion(DocExpansion.None);
-    o.DefaultModelExpandDepth(0);
-});
+app.UseFastEndpoints(s => s.SerializerOptions = o => o.PropertyNamingPolicy = null);
+app.UseOpenApi();
+app.UseSwaggerUi3(c => c.ConfigureDefaults());
 
 await DB.InitAsync(database: "MiniDevTo", host: "localhost");
 _001_seed_initial_admin_account.SuperAdminPassword = app.Configuration["SuperAdminPassword"];

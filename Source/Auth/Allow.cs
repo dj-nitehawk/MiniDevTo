164568b7e751ebff83dadeaed6bb6a6b4f5b7acd
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2023-09-08 10:40:25 +0530

    upgrade pkgs

--- Source/Auth/Allow.cs
+++ Source/Auth/Allow.cs
@@ -6,7 +6,7 @@ public static partial class Allow
     public const string Article_Update = "101";
     public const string Author_Update = "102";
 
-    static partial void Categorize()
+    static partial void Groups()
     {
         AddToAuthor(Author_Update);
 
